50164d9f20ba5cc87261974088c23bf2fdbfe768
riandwicahyo <riandwicahyo28@gmail.com>
2023-01-06 22:25:57 +0700

    add order screen

--- lib/ui/screens/order_screen.dart
+++ lib/ui/screens/order_screen.dart
@@ -5,23 +5,60 @@ import 'package:provider/provider.dart';
 
 import '../../providers/orders.dart';
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   static const routeName = '/orders';
   const OrderScreen({Key? key}) : super(key: key);
 
   @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  Future? _orderFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _orderFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  // bool _isInit = true;
+  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Order'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderWidgetItem(
-          order: orderData.orders[i],
-        ),
+      body: FutureBuilder(
+        future: _orderFuture,
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.error != null) {
+              return const Center(child: Text('Ga ada orderan'));
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) {
+                  return ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => OrderWidgetItem(
+                      order: orderData.orders[i],
+                    ),
+                  );
+                  // return Container();
+                },
+              );
+            }
+            // return const Center(child: Text('Ga ada transaksi'));
+          }
+        },
       ),
     );
   }
